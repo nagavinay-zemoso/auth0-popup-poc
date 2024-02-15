f913a0880580028a21564408e2d19f05400c2966
Nagavinay Chinthada <nagavinay.chinthada@zemosolabs.com>
2024-02-15 11:08:06 +0530

    updated files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/auth0-popup-poc",
+  base: "/auth0-popup-poc/",
   plugins: [react()],
-})
+});
